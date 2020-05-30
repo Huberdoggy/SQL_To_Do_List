0092b32a1fe8fe1751eeb1cb503c75b3e2096867
Kyle Huber <huberdoggy@gmail.com>
2020-05-29 22:29:38 -0500

    working outthe nth child logic, currently working with row transforms green for first to-do item. click trigger for strike through line on any completed item. POST route clears forms on successful add

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,21 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('jQuery sourced.');
     refreshTasks();
     addClickHandlers();
-  });
-
+});
+let colorAlert = '';
 //For create button
 function addClickHandlers() {
     $('#createBtn').on('click', handleCreate);
     $('#taskList').on('click', '.completeTask', completeTask);
     $('#taskList').on('click', '.deleteTask', deleteTask);
-    $('#taskList').on('click', '.completeTask', function () {
-        alert = $(this).html();
-        //change color when clicked
-        $(this).css('background-color', 'green');
-        
-    });
+    $('#taskList').on('click', '.completeTask', successColor);
+
 }
+
+function successColor() {
+    colorAlert = $(this).html();
+    $(this).addClass('on_success');
+    $('table > tbody > tr:nth-child(1)').children().css("background-color","limegreen");
+} //change color when clicked
 
 function handleCreate() {
     console.log('Create button clicked.');
@@ -23,85 +25,88 @@ function handleCreate() {
     tasks.name = $('#taskInput').val();
     tasks.duedate = $('#dueDateInput').val();
     addTask(tasks);
-  }
+}
 
 // refreshTasks will get all tasks from the server and render to page
 function refreshTasks() {
     $.ajax({
-      type: 'GET',
-      url: '/tasks'
-    }).then(function(response) {
-      console.log(response);
-      renderTasks(response);
-    }).catch(function(error){
-      console.log('error in GET', error);
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log(response);
+        renderTasks(response);
+    }).catch(function (error) {
+        console.log('error in GET', error);
     });
 }
-  
+
 // Displays an array of tasks to the DOM
 function renderTasks(tasks) {
     $('#taskList').empty();
-  //empty table before for loop
-    for(let i = 0; i < tasks.length; i += 1) {
-      let task = tasks[i];
-      // For each task, append a new row to our table
-      let $tr = $('<tr></tr>');
-      $tr.data('currentTask', task);
-      $tr.append(`<td>${task.name}</td>`);
-      $tr.append(`<td>${task.duedate}</td>`);
-      $tr.append(`<td><button class="completeTask" data-id="${task.id}">Complete</button></td>`);
-      $tr.append(`<td><button class="deleteTask" data-id="${task.id}">Delete</button></td>`);
-      $('#taskList').append($tr);
+    //empty table before for loop
+    for (let i = 0; i < tasks.length; i += 1) {
+        let task = tasks[i];
+        // For each task, append a new row to our table
+        let $tr = $('<tr></tr>');
+        $tr.data('currentTask', task);
+        $tr.append(`<td>${task.name}</td>`);
+        $tr.append(`<td>${task.duedate}</td>`);
+        $tr.append(`<td><button class="completeTask" data-id="${task.id}">Complete</button></td>`);
+        $tr.append(`<td><button class="deleteTask" data-id="${task.id}">Delete</button></td>`);
+        $('#taskList').append($tr);
     }
-  }
+}
 
 
 function addTask(taskToAdd) {
-  // adds a task to the database
+    // adds a task to the database
     $.ajax({
-      type: 'POST',
-      url: '/tasks',
-      data: taskToAdd,
-      }).then(function(response) {
+        type: 'POST',
+        url: '/tasks',
+        data: taskToAdd,
+    }).then(function (response) {
         console.log('Response from server.', response);
         refreshTasks();
-      }).catch(function(error) {
+        $('#taskInput').val('');
+        $('#dueDateInput').val('');
+    }).catch(function (error) {
         console.log('Error in POST', error);
         alert('Unable to add task at this time. Please try again later.');
-      });
-  }    
+    });
+}
 
-  function completeTask() {
+function completeTask() {
     let buttonElement = $(this);
     let id = $(buttonElement).data('id');
     console.log('completeTask', id);
     $.ajax({
-      method: 'PUT',
-      url: `/tasks/${id}`,
-      data: {
-        checklist: 'Complete'
-      }
+        method: 'PUT',
+        url: `/tasks/${id}`,
+        data: {
+            checklist: 'Complete'
+        }
     }).then((response) => {
-      refreshTasks();
+        successColor();
+        refreshTasks();
     }).catch((err) => {
-      console.log("error: ", err);
+        console.log("error: ", err);
     });
-  }
-  
-  //function deleteTask
-  function deleteTask() {
+}
+
+//function deleteTask
+function deleteTask() {
     let buttonElement = $(this);
     let id = $(buttonElement).data('id');
-      console.log('deleteTask', id);
+    console.log('deleteTask', id);
     $.ajax({
-      type: 'DELETE',
-      url: `/tasks/${id}`
-    })
-      .then((response) => {
-        console.log("delete success!");
-        refreshTasks();
-      })
-      .catch((err) => {
-        console.log("delete failed :-(", err);
-      });
-  }
+            type: 'DELETE',
+            url: `/tasks/${id}`
+        })
+        .then((response) => {
+            console.log("delete success!");
+            refreshTasks();
+        })
+        .catch((err) => {
+            console.log("delete failed :-(", err);
+        });
+}
